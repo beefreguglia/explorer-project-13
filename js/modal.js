1468f86a54af5a675c7183ce3641fb53d6c5d6ad
Bernardo <beefreguglia@gmail.com>
2024-06-21 11:02:21 -0300

    feature: create js files

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal h2'),
+  btnClose: document.querySelector('.modal button.close'),
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
+}
+
+
+Modal.btnClose.onclick = () => {
+  Modal.close()
+}
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    Modal.close()
+  }
+})
+
+export { Modal }
